162a5ec3373d23e9bc35f95262bf012fb388b0e3
liuyangjun <liuyangjun@zhuanzhuan.com>
2025-03-12 17:02:36 +0800

    正则兼容

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.liuyangjun"
-version = "1.1.0"
+version = "1.1.1"
 
 repositories {
 //    mavenCentral()
@@ -42,7 +42,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("242.*")
+        untilBuild.set("251.*")
     }
 
     signPlugin {
